511583fd27a42b0abb3e76142f1907091bd2812a
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-08-20 17:49:18 -0400

    Create an Hash

--- iterations.rb
+++ iterations.rb
@@ -1,9 +1,25 @@
-[1,2,3].each {|i| puts i}
+# [1,2,3].each {|i| puts i}
 
-[1,2,3].each do |i|
-  if i % 2 == 0 
-    puts "#{i} is even."
-  else  
-    puts "#{i} is odd."
-  end 
-end 
+# [1,2,3].each do |i|
+#   if i % 2 == 0 
+#     puts "#{i} is even."
+#   else  
+#     puts "#{i} is odd."
+#   end 
+# end 
+
+
+# 1.upto 3 do |x|
+#   puts x 
+# end 
+
+# 1.upto (3) {|x| puts x}
+
+# Creating a Hash 
+people = Hash.new 
+people[:nickname] = 'Matz'
+people[:language] = 'Japanese'
+people['last name'.intern] = 'Matsumoto'
+people[:nickname]
+people['nickname'.intern]
+puts people 
---
+++ test.rb
@@ -0,0 +1,25 @@
+[1,2,3].each do |i|
+  if i % 2 == 0
+    puts "#{i} is even"
+  else  
+    puts "#{i} is odd"
+  end 
+end 
+
+
+# [1,2,3,4,5,6,7,8,9].each do |i|
+#   if i % 2 == 0 
+#     puts "#{i} is even"
+#   else  
+#     puts "#{i} is odd"
+#   end 
+# end 
+
+
+# [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].each do |i|
+#   if i % 2 == 0 
+#     puts "#{i}  is even"
+#   else  
+#      puts "#{i} is odd"
+#   end 
+# end 
